c4f697312cbaf86de259575e423b496ff094ff76
Ahtesham Abdul Aziz <ahtesham2000@gmail.com>
2024-11-27 16:01:34 +0530

    change search form

--- themes/Mytravel/Template/Views/frontend/blocks/form-search-all-service/style_3.blade.php
+++ themes/Mytravel/Template/Views/frontend/blocks/form-search-all-service/style_3.blade.php
@@ -36,7 +36,7 @@
                 <div class="tab-content hero-tab-pane">
                     @if(!empty($service_types))
                         @php $number = 0; @endphp
-                        @foreach ($service_types as $service_type)
+                        @foreach (array_reverse($service_types) as $service_type)
                             @php
                                 $allServices = get_bookable_services();
                                 if(empty($allServices[$service_type])) continue;
