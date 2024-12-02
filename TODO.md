ec1b80266bfd052eaddb5c9c30f95ba10a99f402
Ahtesham Abdul Aziz <ahtesham2000@gmail.com>
2024-12-02 17:41:44 +0530

    commit: section titles

--- TODO.md
+++ TODO.md
@@ -2,7 +2,7 @@ Space before Event -DONE
 Listing card details -> property size remove, (Bedroom, Beds, People, Bathroom) - DONE
 Listing -> add No of Bedrooms in Extra Info - DONE
 Change icons for all extra info - DONE
-remove google plus, add instagram
-add social media
+remove google plus, add instagram - DONE
+add social media - DONE
 
 
--- themes/Mytravel/Space/Views/frontend/blocks/list-space/style_1.blade.php
+++ themes/Mytravel/Space/Views/frontend/blocks/list-space/style_1.blade.php
@@ -1,16 +1,11 @@
 <div class="bravo-list-space product-card-block product-card-v2 border-bottom border-color-8">
     <div class="container space-1">
         @if(!empty($title))
-            <div class="d-flex justify-content-between mb-3 pt-md-3 mt-1 pb-md-3 mb-md-2 align-items-end">
-                <div class="font-weight-bold font-size-xs-20 font-size-30 mb-0 text-lh-sm">
-                    {{$title}}
-                    <small class="font-size-xs-14 font-size-14 mb-0 text-lh-sm d-block mt-1">
-                        {{ $desc }}
-                    </small>
-                </div>
-                <a href="{{ route("space.search") }}" class="font-weight-bold d-flex text-lh-1 mb-md-2 ml-2">{{ __("More") }}
-                    <i class="flaticon-right-arrow ml-2"></i>
-                </a>
+            <div class="w-md-80 w-lg-50 text-center mx-md-auto mb-5 mt-4">
+                <h2 class="section-title text-black font-size-30 font-weight-bold mb-0">{{$title}}</h2>
+                <small class="font-size-xs-14 font-size-14 mb-0 text-lh-sm d-block mt-1">
+                    {{ $desc }}
+                </small>
             </div>
         @endif
         <div class="row">
@@ -19,6 +14,12 @@
                     @include('Space::frontend.layouts.search.loop-grid')
                 </div>
             @endforeach
+        </div>
+
+        <div class="w-md-80 w-lg-50 text-center mx-md-auto mb-5 mt-4">
+            <a class="p-2 font-size-17 text-center text-white font-weight-bold" href="{{ route('space.search') }}"">
+                Explore more <i class="flaticon-right-arrow ml-2"></i>
+            </a>
         </div>
     </div>
 </div>
